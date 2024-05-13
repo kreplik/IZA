b0779ee90220245213daa730ce5ed7004d73b1a9
adamnieslanik <xniesl00@vutbr.cz>
2024-05-13 11:11:36 +0200

    Initial Commit

---
+++ IZA/IZAApp.swift
@@ -0,0 +1,32 @@
+//
+//  IZAApp.swift
+//  IZA
+//
+//  Created by Adam Nieslanik on 13.05.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct IZAApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
